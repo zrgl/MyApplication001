988d29f7cffe25712aea8554335fdb8ab5d0ddce
zrgl <1234567@qq.com>
2018-10-29 10:51:48 +0800

    第五次提交

---
+++ deamo/src/main/java/com/example/zrgl/deamo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.zrgl.deamo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+         //提交到本地git  add
+
+    }
+}
