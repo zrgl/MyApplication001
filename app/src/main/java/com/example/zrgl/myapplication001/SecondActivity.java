c0ed973e83fb7e9ab20c6940d581e653575ed87f
zrgl <1234567@qq.com>
2018-10-29 09:35:08 +0800

    第一次提交

---
+++ app/src/main/java/com/example/zrgl/myapplication001/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.example.zrgl.myapplication001;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+
+
+    }
+}
